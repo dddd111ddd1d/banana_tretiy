a5a7b3f194771ecfc3727a7f62f6c8b3ddb43510
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-12 09:11:22 +0000

    sS

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import requests
 
 class city:
   def __init__(self, city):
-    r = requests/get("geodb-free-service.wirefreethought.com/v1/geo/cities?limit=1&offset=0&namePrefix=")
+    r = requests.get("http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=1&offset=0&namePrefix=")
     res = r.json()
-    
+    self.name = res['data'][0]['name']
