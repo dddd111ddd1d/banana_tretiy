dcbe63b7b0b2436bead2c54a34c9d694cd0ddf50
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-12 09:38:10 +0000

    dsfsdf

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ class City:
     r = requests.get("http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=1&offset=0&namePrefix=" + city)
     res = r.json()
     self.name = res["data"][0]["name"]
-    self.lat = res[""]
-    self.lon = res[""]
+    self.lat = res["data"][0]["latitude"]
+    self.lon = res["data"][0]["latitude"]
 
+    
 c = input("vedit misto:")
-
 city = City(c)
 
 print(city.name)
+print(city.lat)
+print(city.long)
