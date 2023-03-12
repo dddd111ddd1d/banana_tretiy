a618e245ac82d4c1fca0f7325eb9c7226f96d148
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-12 09:11:55 +0000

    A

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ class city:
     r = requests.get("http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=1&offset=0&namePrefix=")
     res = r.json()
     self.name = res['data'][0]['name']
+
+
+c = input("vedit misto")
